ca97c68507135b9001b973afbba04236f73bd177
Jiles <632036801@qq.com>
2018-11-17 21:40:45 +0800

    Initial Commit

---
+++ JXRaceLamp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXRaceLamp
+//
+//  Created by 徐沈俊杰 on 2018/11/17.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXRaceLamp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXRaceLamp
+//
+//  Created by 徐沈俊杰 on 2018/11/17.
+//  Copyright © 2018年 JX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
